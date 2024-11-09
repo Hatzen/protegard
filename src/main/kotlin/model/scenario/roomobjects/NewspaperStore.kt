1ec0262faa3789b7f77804d852493ab0dfd2c624
Hatzen <kaihartz@gmx.net>
2024-11-09 22:41:17 +0100

    add missing roomobject

---
+++ src/main/kotlin/model/scenario/roomobjects/NewspaperStore.kt
@@ -0,0 +1,22 @@
+package org.example.model.scenario.roomobjects
+
+import model.RoomObject
+import org.example.controller.GameController
+import org.example.model.scenario.Characters
+import java.util.*
+
+class NewspaperStore : RoomObject("Newsstand") {
+    private val newspaperCounter = HashMap<Date, List<String>>()
+    private val MAX_PER_DAY = 3
+
+    override fun interact() {
+        val date = GameController.environment.dateTime
+        val list = newspaperCounter.getOrPut(date) { mutableListOf() }
+        if (list.size >= MAX_PER_DAY) {
+            GameController.addDialog("Nothing new.. " + list.random(), Characters.NARRATOR)
+            return
+        }
+        val text = GameController.randomAnswerController.getRandomNewspaper(date)
+        GameController.addDialog("You have a look at the newspaper" + text, Characters.NARRATOR)
+    }
+}
